98588b5f8a70ee792a693144bdae23f034449851
Jeff Cook <jeff@deserettechnology.com>
2011-11-06 14:15:00 -0700

    Automatically set deleted flag where needed

--- raggregate/rg_migrations/versions/003_Add_deleted_column_to_comments_table.py
+++ raggregate/rg_migrations/versions/003_Add_deleted_column_to_comments_table.py
@@ -1,12 +1,22 @@
 from sqlalchemy import *
+from sqlalchemy import orm
 from migrate import *
 
+from raggregate.models import Comment
 
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
+    smaker = orm.sessionmaker(bind=migrate_engine)
+    db = smaker()
     comments = Table('comments', meta, autoload=True)
     deletedc = Column('deleted', Boolean, default=True)
     deletedc.create(comments)
+    print "Updating deleted flag on existing comments..."
+    for c in db.query(Comment).all():
+        if c.body == '[deleted]':
+            c.deleted = True
+            db.add(c)
+    db.commit()
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
