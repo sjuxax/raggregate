c6d278c1ef0a7d2c03fbbf13aba897514ff52d3a
Jeff Cook <jeff@deserettechnology.com>
2012-05-05 16:02:55 -0600

    Commit forgotten section queries file.

---
+++ raggregate/new_queries/section.py
@@ -0,0 +1,25 @@
+from raggregate.models.submission import Submission
+from raggregate.models.comment import Comment
+from raggregate.models.vote import Vote
+from raggregate.models.section import Section
+
+from raggregate import queries
+
+import sqlahelper
+
+dbsession = sqlahelper.get_session()
+
+def get_sections(parent = None):
+    if parent:
+        r = dbsession.query(Section).filter(Section.parent == parent).all()
+    else:
+        r = dbsession.query(Section).filter(Section.parent == None).all()
+
+    return r
+
+def get_section_by_id(id):
+    return dbsession.query(Section).filter(Section.id == id).one()
+
+def get_section_by_name(name):
+    return dbsession.query(Section).filter(Section.name == name).one()
+
