e72ca58ff4c3244c8c1a8cea93e64e607072d7b5
Jeff Cook <jeff@deserettechnology.com>
2012-06-17 02:16:36 -0600

    subscriptions: Clean up get_subscribed_by_user_id()

--- raggregate/new_queries/subscribe.py
+++ raggregate/new_queries/subscribe.py
@@ -19,19 +19,18 @@ def get_subs_by_section_id(id):
     # Returns a list of subscription objects filtered by section id
     return dbsession.query(Subscribe).filter(Subscribe.section_id == id).all()
 
+def get_base_subscriptions():
+    return dbsession.query(Section.id).filter(Section.subscribe_by_default == True).all()
+
 def get_subscribed_by_user_id(id):
     # Returns a list of section id's that this user is subscribed to
     # It has to generate one list from two data sources
-    subscribed_to_list = []
+    subscribed_to_list = get_base_subscriptions()
     user_subs = get_subs_by_user_id(id)
-    sections = section_queries.get_sections()
-    for sec in sections:
-        if sec.subscribe_by_default:
-            subscribed_to_list.append(sec.id)
     for sub in user_subs:
-        if sub.subscription_status == True:
+        if sub.subscription_status and sub.section_id not in subscribed_to_list:
             subscribed_to_list.append(sub.section_id)
         else:
-            if sub.section_id in subscribed_to:
+            if sub.section_id in subscribed_to_list:
                 subscribed_to_list.remove(sub.section_id)
     return subscribed_to_list
