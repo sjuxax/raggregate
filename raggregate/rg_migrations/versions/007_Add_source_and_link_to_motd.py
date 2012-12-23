0d2af570374fcf1e2e7ee4b0618f99dee413a979
Jeff Cook <jeff@deserettechnology.com>
2012-12-23 00:51:49 -0700

    motd: Zero out migration 007. ...
    Apparently not needed with the model that got pushed out.

--- raggregate/rg_migrations/versions/007_Add_source_and_link_to_motd.py
+++ raggregate/rg_migrations/versions/007_Add_source_and_link_to_motd.py
@@ -3,16 +3,10 @@ from migrate import *
 from migrate.changeset.constraint import UniqueConstraint
 
 def upgrade(migrate_engine):
-    meta = MetaData(bind=migrate_engine)
-    motd = Table('motd', meta, autoload=True)
-    sourcec = Column('source', UnicodeText)
-    sourcec.create(motd)
-    linkc = Column('link', UnicodeText)
-    linkc.create(motd)
+    print "no-op"
+    return True
 
 
 def downgrade(migrate_engine):
-    meta = MetaData(bind=migrate_engine)
-    motd = Table('motd', meta, autoload=True)
-    motd.c.source.drop()
-    motd.c.link.drop()
+    print "no-op"
+    return True
